c395315fa67933a2a43ccc3fccd840336ef88643
Patrick Nisperos <p.nisperos27@gmail.com>
2023-03-04 18:04:17 -0800

    Added foundational database functionality to allow user to write to database and retrieve from database: the 3 search boxes

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,44 +1,59 @@
-# How to Run
-    # 1) Enter src/backend directory
-
-    # 2) Activate database
-
-    # 3) Activate website
-        # unix> python3 app.py
-        # windows> python app.py
-
 import os
+import logging
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend/templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+
+# Set up logging
+logging.basicConfig(filename='error.log', level=logging.ERROR, format='%(asctime)s %(levelname)s: %(message)s')
+
 with app.app_context():
     db = SQLAlchemy(app)
 
+# Create User model with columns for the attributes
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True) # User_ID Column
+    aas_searches = db.Column(db.String(300), nullable=False) # "album_artist_song_search" Column
     song_favorites = db.Column(db.String(300), nullable=False) # Song Favorites Column
     artist_favorites = db.Column(db.String(300), nullable=False) # Artist Favorites Column
     album_favorites = db.Column(db.String(300), nullable=False) # Album Favorites Column
 
+    def __init__(self, aas_searches="", song_favorites="", artist_favorites="", album_favorites=""):
+        self.aas_searches = aas_searches
+        self.song_favorites = song_favorites
+        self.artist_favorites = artist_favorites
+        self.album_favorites = album_favorites
+
     def __repr__(self):
         return '<User_id %r>' % self.user_id
 
-@app.route('/', methods=['POST', 'GET']) # Methods are used in if statements below
+# Define route for main page (search_page)
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST': # If use clicks 'SURF', then bring them to song reccomended page
-        #aas_search = [] # array to store "album_artist_song_search" values
-        #aas_search.append(request.form['content'])
-        #print(aas_search)
-        return render_template('reccomend_page.html')
-    else:
-        return render_template('index.html')
+    if request.method == 'POST': # If user clicks SURF
+        aas_searches = [] # create an empty list for storing the user's AAS searches
+        
+        # Get the values from each input field and add them to the list
+        aas_searches.append(request.form['user_choice1']) # aas = album_artist_song_search
+        aas_searches.append(request.form['user_choice2'])
+        aas_searches.append(request.form['user_choice3'])
+        
+        # Create a new User object with the AAS searches and add it to the database
+        new_user = User(aas_searches=str(aas_searches))
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            app.logger.info('Successfully added user with AAS searches: %s', aas_searches)
+        except Exception as e:
+            app.logger.error('Issue adding user with AAS searches %s. Error: %s', aas_searches, str(e))
+            print(e)
 
+        return render_template('reccomend_page.html', user=new_user)
 
-    
-
+    else: # If user visits the page
+        return render_template('search_page.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/backend/create_db.py
+++ src/backend/create_db.py
@@ -1,4 +1,7 @@
 # run this script to create the database if not already existings within 'instance'
 from app import app, db
-app.app_context().push()
-db.create_all()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
