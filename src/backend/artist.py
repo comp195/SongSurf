d54f8d1eaa3de58b4fb7bb0252f9f5e436c3e307
William Balbuena <w_balbuena@u.pacific.edu>
2023-03-17 02:21:34 -0700

    Added API call to artist.py.  Successfully requests top artists and prints them in json format.

--- src/backend/artist.py
+++ src/backend/artist.py
@@ -1,1 +1,26 @@
-# Artist class backend
+import requests # pip install requests
+import json
+
+def jprint(obj):
+	trext = json.dumps(obj, sort_keys=True, indent=4)
+	print(text)
+
+### IMPORTANT FOR API USE ###
+USER_AGENT = 'wbuop'
+API_KEY = 'ebc5386ea6b0af15cae300e0da5a3af5'
+
+headers = {
+	'user-agent': USER_AGENT
+}
+
+payload = {
+	'api_key': API_KEY,
+	'method': 'chart.gettopartists',
+	'format': 'json'
+}
+##############################
+
+r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
+r.status_code
+
+print(r.json())
