8038f85b57a941b491a42b6fef5b5f99b20f5db5
William Balbuena <w_balbuena@u.pacific.edu>
2023-03-17 02:37:13 -0700

    artist.py requests the top 10 artists for a specific tag (currently rap) and prints the results in the console.

--- src/backend/artist.py
+++ src/backend/artist.py
@@ -1,26 +1,31 @@
 import requests # pip install requests
-import json
 
-def jprint(obj):
-	trext = json.dumps(obj, sort_keys=True, indent=4)
-	print(text)
-
-### IMPORTANT FOR API USE ###
+### IMPORTANT API INFO ###
 USER_AGENT = 'wbuop'
 API_KEY = 'ebc5386ea6b0af15cae300e0da5a3af5'
 
 headers = {
 	'user-agent': USER_AGENT
 }
+###########################
+
+tag = 'rap'
+limit = 10
 
 payload = {
 	'api_key': API_KEY,
-	'method': 'chart.gettopartists',
-	'format': 'json'
+	'method': 'tag.gettopartists',
+	'format': 'json',
+	'limit': limit,
+	'tag': tag
 }
-##############################
+
 
 r = requests.get('https://ws.audioscrobbler.com/2.0/', headers=headers, params=payload)
-r.status_code
-
-print(r.json())
+if r.status_code == 200:
+	data = r.json()
+	artists = data['topartists']['artist']
+	for artist in artists:
+		print(artist['name'])
+else:
+	print(f'Request failed with status code {r.status_code}')
